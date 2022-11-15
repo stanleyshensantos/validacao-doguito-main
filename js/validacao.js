741677f22e00f0c95f1952ce7280c26d18bdf38c
Staney Shen <stanley_shen@hotmail.com>
2022-11-14 21:50:22 -0300

    https://github.com/stanleyshensantos/validacao-doguito-main

--- js/validacao.js
+++ js/validacao.js
@@ -4,6 +4,29 @@ export function valida(input){
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input)
     }
+    if(input.validity.valid){
+        input.parentElement.classList.remove('input-container--invalido')
+    }else{
+        input.parentElement.classList.add('input-container--invalido')
+    }
+}
+const mensagensDeErro = {
+   nome: {
+    valueMissing:'O campo nome não pode estar vazio.'
+   },
+   email:{
+    valueMissing:'O campo email não pode estar vazio.',
+    typeMismatch: 'O email digitado não é válido'
+   },
+   senha:{
+    valueMissing:'O campo senha não pode estar vazio',
+    patterMismatch:'a senha deve conter entre 6 a 12 caracteres deve conter pelo menos uma letra maiuscula, um numero e não deve conter simbolos'
+   },
+   DataNascimento: {
+    valueMissing:'O campo data de nascimento nao pode estar vazio',
+    customError:'Voce de ser maior que 18 anos para se cadastrar.'
+   }
+    
 }
 const validadores= {
     DataNascimento:input => validaDataNascimento(input)
